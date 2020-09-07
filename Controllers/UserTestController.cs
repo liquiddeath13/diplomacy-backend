b270889ebe08eac5a5187cde6239d33d82872d2b
dead inside <securedmiui@gmail.com>
2020-09-07 13:41:29 +0400

    first commit

---
+++ Controllers/UserTestController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace diplomacy_webapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserTestController : ControllerBase
+    {
+        private TestClass tc;
+        private readonly ILogger<UserTestController> _logger;
+
+        public UserTestController(ILogger<UserTestController> logger)
+        {
+            _logger = logger;
+            tc = new TestClass();
+        }
+
+        private User getUserByID(int userID) => tc.UserList.Where(x => x.ID == userID).First();
+
+        private Test getTestByID(int userID, int testID) => getUserByID(userID).OwnTests[testID - 1];
+
+        private TestSolution getSolutionByID(int userID, int solutionID) => getUserByID(userID).OwnSolutions[solutionID];
+
+        private bool validateUserID(int userID) => userID > 0 && userID <= tc.UserList.Count;
+
+        private bool validateTestID(int userID, int testID) => testID > 0 && testID <= getUserByID(userID).OwnTests.Count;
+
+        [HttpGet("tests")]
+        public IEnumerable<Test> GetTestsList(int userID) => validateUserID(userID) ? getUserByID(userID).OwnTests : null;
+
+        [HttpGet("solution")]
+        public TestSolution GetTestSolution(int userID, int testID) => validateUserID(userID) && validateTestID(userID, testID) ? getSolutionByID(userID, testID) : null;
+
+        [HttpGet("test")]
+        public Test GetTest(int userID, int testID) => validateUserID(userID) && validateTestID(userID, testID) ? getTestByID(userID, testID) : null;
+
+        [HttpGet("user")]
+        public User GetUser(int userID) => validateUserID(userID) ? getUserByID(userID) : null;
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace diplomacy_webapi
+{
+    public class TestClass
+    {
+        private static readonly Random getrandom = new Random();
+
+        public static int GetRandomNumber(int min, int max)
+        {
+            lock (getrandom)
+            {
+                return getrandom.Next(min, max);
+            }
+        }
+        public List<User> UserList { get; set; }
+        public TestClass()
+        {
+            UserList = new List<User>();
+            User SampleUser = new User 
+            {
+                ID = 1,
+                Name = "Test User",
+                OwnSolutions = new List<TestSolution>(),
+                OwnTests = new List<Test>()
+            };
+            for (int i = 0; i < 10; i++)
+            {
+                Test NewTest = new Test
+                {
+                    Name = $"Test #{i + 1}",
+                    CreatedAt = DateTime.Now,
+                    EditedAt = DateTime.Now,
+                    Description = $"Automatically created test by Web API ({i + 1})",
+                    TaskList = new List<Task>()
+                };
+                for (int j = 0; j < 15; j++)
+                {
+                    Task NewTask = new Task
+                    {
+                        Name = $"Answer #{j + 1}",
+                        Type = TaskTypes.OneAnswer,
+                        Description = $"Automatically created task by Web API ({j + 1})",
+                        AnswerList = new List<Answer>()
+                    };
+                    int TrueAnswerIndex = GetRandomNumber(0, 4);
+                    for (int k = 0; k < 4; k++)
+                    {
+                        NewTask.AnswerList.Add(new Answer
+                        {
+                            Description = $"Automatically created answer by Web API ({k + 1})",
+                            Flag = k == TrueAnswerIndex
+                        });
+                    }
+                    NewTest.TaskList.Add(NewTask);
+                }
+                SampleUser.OwnTests.Add(NewTest);
+            }
+            UserList.Add(SampleUser);
+        }
+    }
+    public enum TaskTypes : int
+    {
+        OneAnswer,
+        MultipleAnswer,
+        Matching,
+        OpenAnswer
+    }
+    public class Answer
+    {
+        public string Description { get; set; }
+        public bool Flag { get; set; }
+    }
+    public class Task
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public TaskTypes Type { get; set; }
+        public List<Answer> AnswerList { get; set; }
+    }
+    public class Test
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime EditedAt { get; set; }
+        public List<Task> TaskList { get; set; }
+    }
+    public class TaskSolution
+    {
+        public Task Task { get; set; }
+        public Answer UserAnswer { get; set; }
+    }
+    public class TestSolution
+    {
+        public string TestName { get; set; }
+        public List<TaskSolution> TaskListSolution { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public double SolutionMark { get; set; }
+    }
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<Test> OwnTests { get; set; }
+        public List<TestSolution> OwnSolutions { get; set; }
+    }
+}
